ad83bbc131432777f977c2eca3bfac379627dba3
John Thuneby <thuneby@yahoo.dk>
2024-01-10 21:57:27 +0100

    fmt

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~> 3.82.0"
+      source  = "hashicorp/azurerm"
+      version = "3.86.0"
     }
     azurecaf = {
       source  = "aztfmod/azurecaf"
